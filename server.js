4b32630ea6696530140eeb52612d272d9a15b36e
ryand88 <ryand8@hotmail.com>
2019-06-03 16:41:10 -0600

    changed static

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const auth = require("./api/auth");
 const user = require("./api/user");
@@ -23,8 +24,13 @@ app.use("/api/auth", auth);
 app.use("/api/user", user);
 app.use("/api/sheets", sheets);
 
+// Set static folder
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.use(express.static("client/public"));
