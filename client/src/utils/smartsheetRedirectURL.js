39d434fabfd48a98dd72ae53a2bbe327b86911e9
ryand88 <ryand8@hotmail.com>
2019-06-04 07:49:30 -0600

    new smartsheet app for development

--- client/src/utils/smartsheetRedirectURL.js
+++ client/src/utils/smartsheetRedirectURL.js
@@ -1,1 +1,1 @@
-export default "https://app.smartsheet.com/b/authorize?formName=fn_authorize&formAction=fa_loadAuthorize&response_type=code&client_id=i54vnrnj2rsr40tvmmb&scope=READ_SHEETS";
+export default "https://app.smartsheet.com/b/authorize?formName=fn_authorize&formAction=fa_loadAuthorize&response_type=code&client_id=kvtcwablsd4z42jkqnu&scope=READ_SHEETS";
