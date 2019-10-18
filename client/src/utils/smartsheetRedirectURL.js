27797542672e8a1ae01ca827b9803e24f6c2827a
ryand88 <ryand8@hotmail.com>
2019-10-17 22:35:35 -0600

    fixed redirect

--- client/src/utils/smartsheetRedirectURL.js
+++ client/src/utils/smartsheetRedirectURL.js
@@ -1,5 +1,5 @@
-// const client_id = "i54vnrnj2rsr40tvmmb"; // prod
-const client_id = "kvtcwablsd4z42jkqnu"; // dev
+const client_id = "i54vnrnj2rsr40tvmmb"; // prod
+// const client_id = "kvtcwablsd4z42jkqnu"; // dev
 
 // export default `https://app.smartsheet.com/b/authorize?formName=fn_authorize&formAction=fa_loadAuthorize&response_type=code&client_id=${client_id}&scope=READ_SHEETS&state=MY_STATE`;
 
